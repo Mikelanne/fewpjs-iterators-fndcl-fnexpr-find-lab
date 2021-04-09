55010c08582392e0fc1d658e1923a88e03def0be
Mikelanne <mikelanne.wood@gmail.com>
2021-04-09 17:10:31 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function superbowlWin(array) {
+    // each object has a year and a result
+    // use find() to test each object to see if the result is a W
+    // return the year the win occured
+    // if none are found, return undefined
+    let winner =  array.find(function(data) {
+        if (data.result === "W") {
+            return true
+        }
+    })
+    if (winner) {
+        return winner.year
+    } else {
+        return undefined
+    }
+}
 
